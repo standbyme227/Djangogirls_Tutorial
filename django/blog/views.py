7f216d272bbdf50598c85aae026d77de58615397
ShinJongMin <standbyme227@gmail.com>
2018-01-30 12:06:45 +0900

    templates폴더 추가 및 settings의 TEPLATE옵션의 DIR항ㅂ목에 해당폴더 경로 추가

--- django/blog/views.py
+++ django/blog/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse
+from django.shortcuts import  render
 
 
 def post_list(request):
-    return HttpResponse('<html><body><h1>Post list</h1><p>Post목록을 보여줄 예정입니다.</p></body>')
+    return render(request, 'blog/post_list.html')
+    #return HttpResponse('<html><body><h1>Post list</h1><p>Post목록을 보여줄 예정입니다.</p></body>')
