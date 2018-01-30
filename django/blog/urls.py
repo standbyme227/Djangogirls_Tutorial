2abcafb790a8867d4ef554a82d4c22d23cc47255
ShinJongMin <standbyme227@gmail.com>
2018-01-30 12:24:38 +0900

    blog/urls를 수정하고, views도 수정해서 /detail이 실행되게 만듬.

--- django/blog/urls.py
+++ django/blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
-#from blog import views 위와 같음 왜냐하만 '.'은 현재위치를 나타내기때문에
+
+# from blog import views 위와 같음 왜냐하만 '.'은 현재위치를 나타내기때문에
 
 urlpatterns = [
     path('', views.post_list),
+    path('detail/', views.post_detail),
 ]
--- django/blog/views.py
+++ django/blog/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import  render
+from django.shortcuts import render
 
 
 def post_list(request):
     return render(request, 'blog/post_list.html')
-    #return HttpResponse('<html><body><h1>Post list</h1><p>Post목록을 보여줄 예정입니다.</p></body>')
+    # return HttpResponse('<html><body><h1>Post list</h1><p>Post목록을 보여줄 예정입니다.</p></body>')
+
+
+def post_detail(request):
+    return render(request, 'blog/post_detail.html')
