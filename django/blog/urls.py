7e9ec7fcf424510d1f20a3b743a37b3b749110e5
ShinJongMin <standbyme227@gmail.com>
2018-01-30 16:50:43 +0900

    post_detail로 이동하는 링크를 url태그를 사용해서 동적으로 생성

--- django/blog/urls.py
+++ django/blog/urls.py
@@ -4,8 +4,9 @@ from . import views
 # from blog import views 위와 같음 왜냐하만 '.'은 현재위치를 나타내기때문에
 
 urlpatterns = [
-    path('', views.post_list),
-    re_path(r'(?P<pk>\d+)/$', views.post_detail),
+    path('list', views.post_list, name='post-list'),
+    # re_path(r'(?P<pk>\d+)/$', views.post_detail),
+    path('post/<int:pk>/', views.post_detail, name='post-detail'),
     # path('detail/', views.post_detail),
     # re.complie(re'(?P<pk>\d+)')
 ]
